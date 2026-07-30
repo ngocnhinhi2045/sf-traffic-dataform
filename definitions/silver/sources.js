84b52ada863d062aa99c65513e3a6d97e760305c
Ngọc Nhi Đặng <dnnhi2045@gmail.com>
2026-07-30 08:12:21 +0000

    Khoi tao toan bo code Silver, Gold va 3 Data Mart cho CSGT.

---
+++ definitions/silver/sources.js
@@ -0,0 +1,73 @@
+declare({
+  schema: "sf_traffic_bronze",
+  name: "ext_police_station"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "ext_speed_limit"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "ext_neighborhood"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "ext_street"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "ext_traffic_signal"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "ext_camera"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "ext_holiday"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "raw_work_zone"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "raw_traffic_incident"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "ext_traffic_flow"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "raw_weather"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "raw_traffic_crash"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "ext_region_incorporatedplace"
+});
+
+declare({
+  schema: "sf_traffic_bronze",
+  name: "ext_station_metadata"
+});
+
+
+
+
